a2eef22cfd1c8b35b50baff8c6baa6c50f18fccf
Duru Favour <favourduru24@gmail.com>
2025-11-15 03:07:40 -0800

    new code update

--- src/components/shared/chat.tsx
+++ src/components/shared/chat.tsx
@@ -18,31 +18,34 @@ const Chat = () => {
           {name: 'Kelvin Kart', text: 'I have been working', time: '9:53', color: 'bg-green-500'},
           {name: 'Kelvin Port', text: 'How did you start', time: '10:53', color: 'bg-pink-500'},
           {name: 'Favour Duru', text: 'I have sent you the link', time: '1:53', color: 'bg-black'},
-          // {name: 'Duru Pristine', text: 'How should i start', time: '12:53', color: 'bg-orange-500'},
-          // {name: 'Kelvin Kart', text: 'I have been working', time: '12:53', color: 'bg-yellow-500'},
-          // {name: 'Kelvin Port', text: 'How did you start', time: '12:53', color: 'bg-red-500'},
-          // {name: 'Duru Pristine', text: 'How should i start', time: '12:53', color: 'bg-orange-500'},
+          {name: 'Duru Pristine', text: 'How should i start', time: '12:53', color: 'bg-orange-500'},
+          {name: 'Kelvin Kart', text: 'I have been working', time: '12:53', color: 'bg-yellow-500'},
+          {name: 'Kelvin Port', text: 'How did you start', time: '12:53', color: 'bg-red-500'},
+          {name: 'Duru Pristine', text: 'How should i start', time: '12:53', color: 'bg-orange-500'},
    ]
 
    const chatMessage = [
-    // {name: 'Favour Duru', text: 'Lorem ipsum dolor, sit amet consectetur.', time: '9:53', color: 'bg-red-500'},
-          // {name: 'Kelvin Kart', text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis tempore eius, ab nulla officia minima!', time: '9:53', color: 'bg-green-500'},
+    {name: 'Favour Duru', text: 'Lorem ipsum dolor, sit amet consectetur.', time: '9:53', color: 'bg-red-500'},
+          {name: 'Kelvin Kart', text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis tempore eius, ab nulla officia minima!', time: '9:53', color: 'bg-green-500'},
           {name: 'Kelvin Kart', text: 'Lorem ipsum dolor, sit amet consectetur ', time: '9:53', color: 'bg-green-500', image: '/image/card2.webp'},
+          {name: 'Kelvin Kart', text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis tempore eius, ab nulla officia minima!', time: '9:53', color: 'bg-green-500'},
    ]
 
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-full overflow-hidden">
      <Header />
       <div className="flex h-full">
          {/* First cols */}
-         <div className="max-w-[70px] w-full flex flex-col p-2 items-center bg-[#F9FBFC] border rounded-l-md">
+         <div className="max-w-[70px] w-full flex flex-col p-2 items-center bg-[#F9FBFC] border rounded-l-md h-full">
+
             <div className="w-8 h-8 rounded-md bg-purple-400 flex items-center justify-center whitespace-nowrap shrink-0">
                <p className="font-semibold text-white">C</p>
             </div>
-            <div className="gap-6 flex flex-col justify-between h-full mt-10 items-center">
+
+            <div className="gap-6 flex flex-col justify-between py-8 mt-5 items-center m-1 h-full">
                <div className="flex flex-col gap-4 ">
                        {navLink.map((nav) => (
-                       <div className="bg-white p-2 rounded-full shadow-sm cursor-pointer">
+                       <div className="hover:bg-white p-2 rounded-full shadow-sm cursor-pointer transition-colors">
                          <nav.icon className="text-[#495568] size-6 font-bold"/>
                        </div>
                 ))}
@@ -50,7 +53,7 @@ const Chat = () => {
 
                <div className="flex flex-col gap-4 justify-center items-center">
                        {navLink.map((nav) => (
-                       <div >
+                       <div className="hover:bg-white p-2 rounded-full shadow-sm cursor-pointer transition-colors">
                          <nav.icon className="text-[#495568] size-6"/>
                        </div>
                 ))}
@@ -69,16 +72,19 @@ const Chat = () => {
                </div>
            </div>
 
-             <div className="flex flex-col bg-[#F9FBFC] h-ful">
+             <div className="flex flex-col bg-[#F9FBFC] h-full">
+              
                 <div className="p-4 h-14 flex items-center">
                    <div className="flex justify-between gap- items-center w-full">
                   <p className="text-lg font-semibold text-[#8B92A1]">Chats <span className="bg-pink-100 py-0.5 px-1.5 w-fit rounded-[20px] text-dark-100 text-sm font-semibold text-pink-400">23</span></p>
                   <Plus className="text-[#495568] size-7 mt-1 font-semibold cursor-pointer"/>
                </div>
                 </div>
-                
-               {chatData.map((chat, index) => (
-                 <div className="p- h-20 flex items-center bg-[#F9FBFC]  cursor-pointer hover:bg-[#8E8AD8]" key={index}>
+                 
+                 <div className="overflow-y-auto scroll-smooth
+                  scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+                      {chatData.map((chat, index) => (
+                 <div className="h-20 flex items-center bg-[#F9FBFC]  cursor-pointer hover:bg-[#8E8AD8] " key={index}>
                 <div className=" bg-[#F9FBFC] w-[99%] h-full p-4 hover:bg-[#EDF2FE]">
                    <div className="flex items-start justify-between w-full">
 
@@ -99,15 +105,17 @@ const Chat = () => {
                 </div>
               </div>
                ))}
+                 </div>
+               
              </div>
 
              
          </div>
 
          {/*Third cols */}
-         <div className="w-full border flex flex-col rounded-r-md">
+         <div className="w-full border flex flex-col rounded-r-md overflow-hidde">
           
-            <div className="p-10 h-20 border flex items-center rounded-r-md">
+            <div className="p-10 h-20 border flex items-center rounded-tr-md">
                <div className="flex justify-between gap- items-center w-full">
                   <div className="flex gap-4">
                     <div className="w-10 h-10 rounded-full bg-purple-400 flex items-center justify-center whitespace-nowrap shrink-0">
@@ -131,14 +139,15 @@ const Chat = () => {
                       <Phone className="text-[#495568] size-6 cursor-pointer"/>
                       </span>
                          <div className="bg-purple-600 p-2 w-fit rounded-full text-dark-100 ">
-                            <Plus className="text-[#495568] size-6 font-semibold cursor-pointer text-white"/>
+                            <Plus className="size-6 font-semibold cursor-pointer text-white"/>
                          </div>
                    </div>
                </div>
            </div>
              {/* want to make here scroll with small scroll */}
-             <div className="p-10 h-full">
-               <div className="h-full flex flex-col gap-5">
+             <div className="px-10 pt-5 pb-10 flex-1 overflow-y-auto scroll-smooth
+                  scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+               <div className="h-ful flex flex-col gap-5">
                {chatMessage.map((chat, index) => (
                 <div className="flex gap-3" key={index}>
                     <div className={`w-12 h-12 rounded-full ${chat.color} flex items-center justify-center whitespace-nowrap shrink-0`}>
@@ -165,10 +174,10 @@ const Chat = () => {
              {/* The End */}
              {/* what to make this form stick at the bottom */}
 
-             <div className="p-10 h-20 border flex items-center rounded-r-md">
-               <div className="flex justify-between items-center w-full">
+             <div className="h-20 flex  rounded-r-md sticky bottom-0 bg-white w-[98%]">
+               <div className="flex justify-between items-center w-[98%] p-2">
                   <form className="max-w-[700px] w-full border-2 border-gray-300 rounded-full flex items-center">
-                 <input type="text" className="w-full rounded-full p-2 outline-none" placeholder="Type Message here..."/>
+                 <input type="text" className="w-full rounded-full p-2 outline-none text-[1rem] font-medium text-gray-500" placeholder="Type Message here..."/>
                    <div className="flex p-2 gap-2">
                     <Mic className="text-[#495568] size-6 font-bold cursor-pointer"/>
                     <Image className="text-[#495568] size-6 font-bold cursor-pointer"/>
@@ -176,7 +185,7 @@ const Chat = () => {
                    </div>
                   </form>
                   <div className="w-12 h-12 rounded-full bg-purple-400 flex items-center justify-center">
-                    <Send className="text-[#495568] size-6 cursor-pointer text-white"/>
+                    <Send className="size-6 cursor-pointer text-white"/>
                     </div>
                    <div>
                    </div>
