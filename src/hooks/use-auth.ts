e0b8ba2808df100d549effbaf96010b77d0e0641
Pristine duru <Favourduru24@gmail.com>
2025-12-01 08:00:25 -0800

    done implementing the logout auth function

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -44,7 +44,6 @@ interface AuthState {
              set({isLoggingIn: true})
 
             try {
-
                 const response = await API.post('/auth/login', data)
                 set({user: response.data.user})
                 useSocket.getState().connectSocket()
@@ -55,9 +54,30 @@ interface AuthState {
             }  
          },
          logout: async () => {
-             
+             try {
+                await API.post("/auth/logout")
+                set({user: null})
+                useSocket.getState().disconnectSocket()
+             } catch (err: any) {
+                toast.error(err.response?.data?.message || 'Logout failed!')
+                
+             }
          },
-         isAuthStatus: () => {}
+         isAuthStatus: async () => {
+                set({isAuthStateLoading: true})
+
+                 try {
+                const response = await API.get('/auth/status')
+                set({user: response.data.user})
+                useSocket.getState().connectSocket()
+            } catch (err: any) {
+                toast.error(err.response?.data?.message || 'Authentication failed!')
+                console.log(err)
+                // set({user: null})
+            } finally {
+                set({isAuthStateLoading: false})
+            }  
+         }
        }),
        {
          name: 'whop:root'
