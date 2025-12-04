566b8741ea32bbbb16243bb60ca2d6a61bf59704
Pristine duru <Favourduru24@gmail.com>
2025-12-03 20:34:13 -0800

    new code update

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom"
 const SignIn = () => {
 
   return (
-    <section className="w-full flex justify-center items-center h-screen">
+    <section className="w-full flex lg:flex-row items-center justify-center h-screen gap-5 px-5">
+
          <div className="w-full max-w-lg flex flex-col gap-6 py-6 bg-white border border-[#ebeeed] rounded-xl shadow-sm p-2 justify-center">
                               <div className="w-full flex flex-col gap-3">
                                 <div className="flex gap-2 justify-center items-center">
@@ -28,13 +29,17 @@ const SignIn = () => {
                  </div>
 
                   <div className="w-full flex flex-col gap-2.5 px-6 relative mt-4">
-                      <button className="w-full bg-purple-800 hover:bg-purple-700 focus:bg-purple-500 rounded-md cursor-pointer">
+                      <button className="w-full bg-purple-800 hover:bg-purple-700 focus:bg-purple-500 rounded-md cursor-pointer h-12">
                         <p className="text-white font-semibold text-xl p-2 ">Sign In</p>
                       </button>
                  </div>
               </form>
                    <p className="text-sm font-medium text-end mx-6">Dont have an account <Link to="/sign-up"><span className="text-purple-600 cursor-pointer">Sign Up</span></Link></p>
          </div>
+
+         <div className="w-full h-full bg-purple-50 hidden lg:block">
+               <img src="/image/auth.png" alt="img" className="w-full h-full object-cover"/>
+             </div>
          
     </section>
   )
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom"
 const SignUp = () => {
 
   return (
-    <section className="w-full flex justify-center items-center h-screen">
-         <div className="w-full max-w-lg flex flex-col gap-6 py-6 bg-white border border-[#ebeeed] rounded-xl shadow-sm p-2 justify-center">
+    <section className="w-full flex lg:flex-row items-center justify-center h-screen gap-5 px-5">
+            
+         <div className="w-full max-w-lg flex flex-col gap-6 py-6  bg-white border border-[#ebeeed] rounded-xl shadow-sm p-2 justify-center ">
                               <div className="w-full flex flex-col gap-3">
                                 <div className="flex gap-2 justify-center items-center">
                                   <div className="w-8 h-8 rounded-md bg-purple-400 flex items-center justify-center">
@@ -33,14 +34,16 @@ const SignUp = () => {
                  </div>
 
                   <div className="w-full flex flex-col gap-2.5 px-6 relative mt-4">
-                      <button className="w-full bg-purple-800 hover:bg-purple-700 focus:bg-purple-500 rounded-md cursor-pointer">
+                      <button className="w-full bg-purple-800 hover:bg-purple-700 focus:bg-purple-500 rounded-md cursor-pointer h-12">
                         <p className="text-white font-semibold text-xl p-2 ">Sign Up</p>
                       </button>
                  </div>
               </form>
                    <p className="text-sm font-medium text-end mx-6">Already have an account <Link to="/"><span className="text-purple-600 cursor-pointer">Sign In</span></Link></p>
          </div>
-         
+             <div className="w-full h-full bg-purple-50 hidden lg:block">
+               <img src="/image/auth.png" alt="img" className="w-full h-full object-cover"/>
+             </div>
     </section>
   )
   
